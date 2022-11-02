4537f77d46e2b02fd581077e4d44ac57582540b0
carmona44 <dcacarmona44@gmail.com>
2022-11-02 20:44:01 +0100

    RESOLVE: deploy contract

---
+++ hardhat/constants/index.js
@@ -0,0 +1,4 @@
+const WHITELIST_CONTRACT_ADDRESS = "0xe28EA7ee81dCA245770ffcBEa50DaAc38A1a8B0B";
+const METADATA_URL = "https://buenachica-whitelist.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
